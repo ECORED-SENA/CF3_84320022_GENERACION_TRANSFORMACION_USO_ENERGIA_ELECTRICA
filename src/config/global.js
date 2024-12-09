9816240153c74a0169abfc6887743ec77c4c1de2
jhonuru <47787645+jhonuru@users.noreply.github.com>
2024-12-09 14:30:19 -0500

    Terminado

--- src/config/global.js
+++ src/config/global.js
@@ -100,31 +100,101 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Definición y clasificación de las máquinas eléctricas rotativas',
+      referencia: 'INFOPLC. (s. f.). Máquinas Eléctricas Rotativas.',
+      tipo: 'Documento',
+      link:
+        'http://www.infoplc.net/files/documentacion/motion_control/infoPLC_net_8448127641.pdf',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Generadores',
+      referencia:
+        'ventageneradores.net. (2011). Como funciona un generador eléctrico?. [Archivo de video] YouTube.  ',
+      tipo: 'Video',
+      link:
+        'https://www.youtube.com/watch?v=vdzpxgVn6N0&ab_channel=ventageneradores.net',
+    },
+    {
+      tema: 'Motores',
+      referencia:
+        'Archivo Software Multimedia. (2019). ¿Como funciona el MOTOR ELÉCTRICO? - Grandes Inventos - Unlimited. [Archivo de video] YouTube.',
+      tipo: 'Video',
+      link:
+        'https://www.youtube.com/watch?v=JurMDGbZ6VU&ab_channel=ArchivoSoftwareMultimedia',
+    },
+    {
+      tema: 'Factor de potencia',
+      referencia:
+        'Sígueme la Corriente. ().¿Qué es el Factor de Potencia? ¿Para qué sirve? ⚡ Sígueme la Corriente. [Archivo de video] YouTube.',
+      tipo: 'Video',
+      link:
+        'https://www.youtube.com/watch?v=_mvA-LP2n_w&ab_channel=S%C3%ADguemelaCorriente',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Conservación de energía',
+      significado:
+        'principio que asegura que la energía no se crea ni se destruye, solo cambia de forma.',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: 'Factor de potencia',
+      significado:
+        'indicador que mide la eficiencia en la conversión de energía eléctrica en trabajo útil.',
+    },
+    {
+      termino: 'Generador',
+      significado: 'máquina que convierte energía mecánica en eléctrica.',
+    },
+    {
+      termino: 'Máquina eléctrica',
+      significado:
+        'dispositivo que transforma o genera energía eléctrica o mecánica.',
+    },
+    {
+      termino: 'Motor',
+      significado: 'máquina que transforma energía eléctrica en mecánica.',
+    },
+    {
+      termino: 'Optimización energética',
+      significado:
+        'proceso de mejorar el uso eficiente de la energía en un sistema.',
+    },
+    {
+      termino: 'Potencia activa',
+      significado:
+        'energía útil que realiza trabajo en un sistema eléctrico, medida en <i>watts</i>.',
+    },
+    {
+      termino: 'Potencia aparente',
+      significado:
+        'total de energía suministrada, suma de la activa y reactiva, medida en VA.',
+    },
+    {
+      termino: 'Potencia reactiva',
+      significado:
+        'energía empleada en la creación de campos eléctricos o magnéticos, medida en VAR.',
+    },
+    {
+      termino: 'Sobrecarga',
+      significado:
+        'exceso de corriente que afecta el rendimiento y vida útil de los equipos eléctricos.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Equipo INIECO. (2011). <i>Electrotecnia</i>. Málaga: Editorial Vértice.',
+      link: '',
+    },
+    {
+      referencia: 'INFOPLC. (s. f.). <i>Máquinas Eléctricas Rotativas</i>. ',
+      link:
+        'http://www.infoplc.net/files/documentacion/motion_control/infoPLC_net_8448127641.pdf',
+    },
+    {
+      referencia:
+        'Wildi, T. (2006). <i>Máquinas eléctricas y sistemas de potencia</i>. México: Pearson Educación.',
       link: '',
     },
   ],
@@ -148,8 +218,14 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
+          nombre: 'Wilmar Martínez Urrutia',
+          cargo: 'Experto temático',
+          centro:
+            'Centro de Desarrollo Agroempresarial - Regional Cundinamarca',
+        },
+        {
+          nombre: 'Paola Alexandra Moya',
+          cargo: 'Evaluadora instruccional',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
       ],
@@ -158,35 +234,35 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Blanca Flor Tinoco Torres',
           cargo: 'Diseñador de contenidos',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Jhon Jairo Urueta Alvarez',
           cargo: 'Desarrollador <i>full stack</i>',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
-        {
-          nombre: 'Nombre',
-          cargo: 'Animador y productor audiovisual',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
-        },
+        //{
+        //  nombre: 'Nombre',
+        //  cargo: 'Animador y productor audiovisual',
+        //  centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        //},
       ],
     },
     {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
-        {
-          nombre: 'Luis Gabriel Urueta Alvarez',
-          cargo: 'Validador y vinculador de recursos educativos digitales',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
-        },
-        {
-          nombre: 'Jaime Hernán Tejada Llano',
-          cargo: 'Validador y vinculador de recursos educativos digitales',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
-        },
+        //{
+        //  nombre: 'Luis Gabriel Urueta Alvarez',
+        //  cargo: 'Validador y vinculador de recursos educativos digitales',
+        //  centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        //},
+        //{
+        //  nombre: 'Jaime Hernán Tejada Llano',
+        //  cargo: 'Validador y vinculador de recursos educativos digitales',
+        //  centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        //},
         {
           nombre: 'Daniel Ricardo Mutis Gómez',
           cargo: 'Evaluador de contenidos inclusivos y accesibles',
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -43,12 +43,12 @@
                     img(src='@/assets/curso/tema1/img1.svg', alt='', style='width: 60px')
                 .col
                   h3.mb-0 Clasificación de las máquinas eléctricas rotativas
-          p.mb-4 La clasificación de las máquinas eléctricas rotativas se basa en su función principal: generadores, que transforman energía mecánica en eléctrica, y motores, que convierten energía eléctrica en mecánica.
+          p.mb-4(data-aos="fade-right") La clasificación de las máquinas eléctricas rotativas se basa en su función principal: generadores, que transforman energía mecánica en eléctrica, y motores, que convierten energía eléctrica en mecánica.
           .row.justify-content-center
             .col-lg-10
               .p-4.bg-c3.shadow
                 .row
-                  .col-lg-6.mb-4.mb-lg-0
+                  .col-lg-6.mb-4.mb-lg-0(data-aos="fade-right")
                     .p-4.bg-c6
                       .row.align-items-end
                         .col-md-auto.mb-4.mb-md-0
@@ -57,7 +57,7 @@
                         .col
                           p #[b Generadores]
                           p.mb-0 Transforman la energía mecánica en energía eléctrica.
-                  .col-lg-6
+                  .col-lg-6(data-aos="fade-left")
                     .p-4.bg-c6
                       .row.align-items-end
                         .col-md-auto.mb-4.mb-md-0
@@ -70,9 +70,9 @@
           #t_1_1
           .titulo-segundo.color-acento-contenido
             h2 1.1 Factor de potencia
-          p La potencia es la velocidad a la que se consume la energía. También se puede definir como la energía desarrollada o consumida en una unidad de tiempo, expresada en la fórmula:
+          p(data-aos="fade-right") La potencia es la velocidad a la que se consume la energía. También se puede definir como la energía desarrollada o consumida en una unidad de tiempo, expresada en la fórmula:
           .row.justify-content-center.mb-4
-            .col-lg-5.mb-4.mb-lg-0
+            .col-lg-5.mb-4.mb-lg-0(data-aos="fade-right")
               .p-4.bg-c7
                 .row.align-items-center.mb-3
                   .col-md-auto
@@ -84,12 +84,12 @@
                     p.mb-0 E: energía (en #[i joules]).
                     p.mb-0 t: tiempo (en segundos).
                 p.mb-0 La potencia eléctrica se define como la energía consumida en un dispositivo por unidad de tiempo.
-            .col-lg-4
+            .col-lg-4(data-aos="fade-left")
               figure
                 img(src='@/assets/curso/tema1/img5.png', alt='', style="width: 340px").m-auto
           .row.mb-4
             .col-12
-              .p-4.bg-c3.shadow
+              .p-4.bg-c3.shadow(data-aos="fade-down")
                 .row.align-items-center
                   .col-lg-6.mb-4.mb-lg-0
                     p.mb-0 El factor de potencia es un término utilizado para cuantificar la cantidad de energía eléctrica que se convirtió en trabajo. El valor ideal del factor de potencia es 1, lo que indica que toda la energía consumida por los aparatos ha sido transformada en trabajo. El factor de potencia se puede representar como:
@@ -97,10 +97,10 @@
                     figure
                       img(src='@/assets/curso/tema1/img4.svg', alt='', style="width: 500px").m-auto
           .row.align-items-center.mb-4
-            .col-lg-5.mb-4.mb-lg-0
+            .col-lg-5.mb-4.mb-lg-0(data-aos="fade-right")
               figure
                 img(src='@/assets/curso/tema1/img6.png', alt='', style="width: 470px").m-auto
-            .col-lg-7
+            .col-lg-7(data-aos="fade-left")
               p Existen tres tipos de potencia:
               AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta--azul1")
                 .row(titulo="Potencia activa (P)")
@@ -112,15 +112,15 @@
                 .row(titulo="Potencia aparente (S)")
                   .col-12
                     p.mb-0 Suma algebraica de las potencias activa y reactiva. Representa la potencia total suministrada y se mide en voltios-amperios (VA).
-    p.mb-4 En la siguiente figura, se presenta una representación geométrica del factor de potencia, donde se relacionan la potencia activa (P), la potencia aparente (S) y el ángulo de desfase (Φ).
+    p.mb-4(data-aos="fade-right") En la siguiente figura, se presenta una representación geométrica del factor de potencia, donde se relacionan la potencia activa (P), la potencia aparente (S) y el ángulo de desfase (Φ).
     .row.justify-content-center.mb-4
       .col-lg-8
-        .titulo-sexto.color-acento-contenido
+        .titulo-sexto.color-acento-contenido(data-aos="fade-right")
           p.mb-0 #[b Figura 1.] Representación geométrica del factor de potencia
-        figure
+        figure(data-aos="fade-left")
           img(src='@/assets/curso/tema1/img5.svg', alt='La figura representa la relación matemática entre la potencia activa (P) y la potencia aparente (S) mediante el factor de potencia, definido como el coseno del ángulo Φ (⁅cos⁆⁅ ⁆⁅Φ⁆⁅ ⁆).', style="width: 800px").m-auto
     .row
-      .col-lg-7.mb-4.mb-lg-0
+      .col-lg-7.mb-4.mb-lg-0(data-aos="fade-right")
         p Las consecuencias de un bajo factor de potencia son:
         .row
           .col-12
@@ -165,7 +165,7 @@
                         i.fas.fa-bolt(style="color: #FF4947")
                         |
                         p.mb-0 Aumento en los costos de facturación eléctrica debido al mayor consumo de corriente.
-      .col-lg-5
+      .col-lg-5(data-aos="fade-left")
         p Los beneficios de corregir el factor de potencia son:
         .row.mb-3
           .col-12
@@ -192,9 +192,9 @@
     #t_1_2
     .titulo-segundo.color-acento-contenido
       h2 1.2 Ejemplos
-    p A continuación, se presentan algunos ejemplos:
+    p(data-aos="fade-right") A continuación, se presentan algunos ejemplos:
     .row
-      .col-lg-6.mb-4.mb-lg-0
+      .col-lg-6.mb-4.mb-lg-0(data-aos="fade-right")
         .row.mb-3
           .col-12
             .p-4.bg-c9
@@ -231,7 +231,7 @@
               p Por lo tanto, = 28.1º
         figure
           img(src='@/assets/curso/tema1/img8.png', alt='', style="width: 600px").m-auto
-      .col-lg-6
+      .col-lg-6(data-aos="fade-left")
         .p-4.bg-c10
           p.mb-0 #[b Ejemplo 2:]
           p Un motor monofásico absorbe una corriente de 5A de una línea de 120V y 60Hz. El factor de potencia del motor es de 65 por ciento.
